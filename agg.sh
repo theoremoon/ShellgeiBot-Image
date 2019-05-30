ca1eda9dd8ae5e3ad04de0978317698fe399ac97
jiro4989 <jiroron666@gmail.com>
2019-05-30 11:57:30 +0900

    :fire: 作業用に作ったファイルを削除

--- agg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-awk '{print $NF, $0}' before.log \
-  | sed -E 's/\s+$/ /g' \
-  | sed -E 's/\s+=>//g' \
-  | grep -E '^[0-9.]+s' \
-  | sed -E 's/^([0-9.]+)s/\1/' \
-  | sort -rnk1 \
-  > times.log
