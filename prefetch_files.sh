4f69bc745208ffb8882b5d3ad0afc0130f985a63
so <horo1717@gmail.com>
2021-10-16 18:09:37 +0900

    update: Ubuntu 21.10

--- prefetch_files.sh
+++ prefetch_files.sh
@@ -8,7 +8,7 @@ DOWNLOAD_DIR=$(cd "$(dirname "$0")"; pwd)/prefetched
 
 # go
 [ -f "$DOWNLOAD_DIR/go.tar.gz" ] || {
-  curl -sfSL --retry 5 https://dl.google.com/go/go1.16.6.linux-amd64.tar.gz -o "$DOWNLOAD_DIR/go.tar.gz"
+  curl -sfSL --retry 5 https://dl.google.com/go/go1.17.2.linux-amd64.tar.gz -o "$DOWNLOAD_DIR/go.tar.gz"
 }
 
 # chromium
@@ -18,10 +18,10 @@ DOWNLOAD_DIR=$(cd "$(dirname "$0")"; pwd)/prefetched
 
 # julia
 [ -f "$DOWNLOAD_DIR/julia.tar.gz" ] || {
-  curl -sfSL --retry 5 "https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.2-linux-x86_64.tar.gz" -o "$DOWNLOAD_DIR/julia.tar.gz"
+  curl -sfSL --retry 5 "https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.3-linux-x86_64.tar.gz" -o "$DOWNLOAD_DIR/julia.tar.gz"
 }
 
 # openjdk
 [ -f "$DOWNLOAD_DIR/openjdk16.tar.gz" ] || {
-  curl -sfSL --retry 5 "https://download.java.net/java/GA/jdk16.0.2/d4a915d82b4c4fbb9bde534da945d746/7/GPL/openjdk-16.0.2_linux-x64_bin.tar.gz" -o "$DOWNLOAD_DIR/openjdk16.tar.gz"
+  curl -sfSL --retry 5 "https://download.java.net/java/GA/jdk17/0d483333a00540d886896bac774ff48b/35/GPL/openjdk-17_linux-x64_bin.tar.gz" -o "$DOWNLOAD_DIR/openjdk17.tar.gz"
 }
